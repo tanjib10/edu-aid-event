f13c447fd7c5e288574c12b42fced94ab9e69fb6
tanjib10 <radiulmd@gmail.com>
2023-10-08 01:09:16 +0600

    Project-Setup

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div className="text-center">
+      <h3 className="text-3xl font-bold">Oops!!</h3>
+      <p className="text-2xl font-semibold">404 Not Found</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./layout/Root";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import Home from "./Home/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
