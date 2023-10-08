d8070360f889adde018ec1b952cfefec0bfba33a
tanjib10 <radiulmd@gmail.com>
2023-10-09 02:21:35 +0600

    added navbar

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div className="text-center">
+    <div className="h-[70vh] flex flex-col justify-center items-center text-center">
       <h3 className="text-3xl font-bold">Oops!!</h3>
       <p className="text-2xl font-semibold">404 Not Found</p>
+      <Link to="/" className="mt-2 font-semibold text-lg">
+        Return <span className="text-blue-600 ">home</span>
+      </Link>
     </div>
   );
 };
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
 };
 
 export default Home;
